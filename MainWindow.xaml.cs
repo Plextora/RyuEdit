8ceff2cf1ec5422206830dc214f19578a342a736
Plextora <71889427+Plextora@users.noreply.github.com>
2023-01-12 16:58:16 -0500

    Added functionality to minimize and close buttons

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Input;
 
 namespace RyuEdit
@@ -16,8 +15,8 @@ namespace RyuEdit
 
         #region Toolbar
 
-        private void CloseButton_Click(object sender, RoutedEventArgs e) => throw new NotImplementedException();
-        private void MinimizeButton_Click(object sender, RoutedEventArgs e) => throw new NotImplementedException();
+        private void CloseButton_Click(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
         private void WindowToolbar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => DragMove();
 
         #endregion
